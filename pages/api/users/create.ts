7eda4e7b2f4692656238cae583774cc11fef1bb7
Daniel Olavio Ferreira <danielolaviobr@gmail.com>
2021-04-10 19:32:57 +0100

    catch create user error

--- pages/api/users/create.ts
+++ pages/api/users/create.ts
@@ -4,23 +4,27 @@ import { User } from "@dtos/user";
 import userExists from "functions/userExists";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { user } = req.body as { user: User };
+  try {
+    const { user } = req.body as { user: User };
 
-  const doesUserExist = await userExists(user.uid);
+    const doesUserExist = await userExists(user.uid);
 
-  if (doesUserExist) {
-    return res.status(200).json(user);
+    if (doesUserExist) {
+      return res.status(200).json(user);
+    }
+
+    const newUser = await prisma.user.create({
+      data: {
+        id: user.uid,
+        name: user.name,
+        avatar: user.photoUrl,
+        email: user.email,
+        provider: user.provider,
+      },
+    });
+
+    return res.status(200).json(newUser);
+  } catch (err) {
+    return res.status(500).json(err);
   }
-
-  const newUser = await prisma.user.create({
-    data: {
-      id: user.uid,
-      name: user.name,
-      avatar: user.photoUrl,
-      email: user.email,
-      provider: user.provider,
-    },
-  });
-
-  return res.status(200).json(newUser);
 };
