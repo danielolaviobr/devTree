8e75839ccab1b04bfed88726ae15e00868eed77d
Daniel Olavio Ferreira <danielolaviobr@gmail.com>
2021-04-10 19:28:04 +0100

    API fix

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:3000/api" });
+const api = axios.create({ baseURL: `${process.env.NEXT_PUBLIC_URL}/api` });
 
 export default api;
