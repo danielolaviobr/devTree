279e723adb506b4c4d8d0ee4eafbb35aba73a5eb
Daniel Olavio Ferreira <danielolaviobr@gmail.com>
2021-04-10 19:57:30 +0100

    prisma validation change

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -7,7 +7,7 @@ declare global {
   }
 }
 let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "development") {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
